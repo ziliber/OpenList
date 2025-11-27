1f373eac8d7d47884345d96c8d98ed01e8fce0ba
Seven <53081179+Seven66677731@users.noreply.github.com>
2025-11-27 14:02:11 +0800

    chore(strm): avoid generating empty folders (#1720)
    chore(strm): empty folders are not generated locally

--- drivers/strm/hook.go
+++ drivers/strm/hook.go
@@ -89,13 +89,7 @@ func RemoveStrm(dstPath string, d *Strm) {
 }
 
 func generateStrm(ctx context.Context, driver *Strm, obj model.Obj, localPath string) {
-	if obj.IsDir() {
-		err := utils.CreateNestedDirectory(localPath)
-		if err != nil {
-			log.Warnf("failed to generate strm dir %s: failed to create dir: %v", localPath, err)
-			return
-		}
-	} else {
+	if !obj.IsDir() {
 		link, err := driver.Link(ctx, obj, model.LinkArgs{})
 		if err != nil {
 			log.Warnf("failed to generate strm of obj %s: failed to link: %v", localPath, err)
