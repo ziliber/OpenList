9835afc6454469f038715879a735efdf2e7621d5
j2rong4cn <36783515+j2rong4cn@users.noreply.github.com>
2025-11-27 19:34:03 +0800

    refactor:  improve upload handling (#1455)
    * fix(quark): refactor upPart to use http.NewRequest
    
    * fix(quark): improved upload handling
    
    * fix(quark_open): improved upload handling
    
    * fix: add retry context to multiple upload functions
    
    * fix: optimize hash calculation in multipart upload to avoid blocking
    
    * fix: update error handling in lifecycle functions for better clarity
    
    * fix: update upload progress calculation to improve accuracy
    
    * fix: simplify error handling in lifecycle functions for improved readability
    
    * fix: remove unnecessary mutex for part uploads to simplify code
    
    * fix(stream): simplify file handling in NewStreamSectionReader and improve error messages
    
    * fix(terabox): optimize chunk count calculation in Put method
    
    * perf(chaoxing): 表单上传文件0拷贝
    
    * fix(cnb_releases): improve file upload progress tracking
    
    * fix(baidu_netdisk): improve upload handling
    
    * fix(upload): optimize buffer initialization for file uploads
    
    * fix(baidu_netdisk): add retry condition to skip ErrUploadIDExpired in upload loop

--- drivers/google_drive/util.go
+++ drivers/google_drive/util.go
@@ -383,6 +383,7 @@ func (d *GoogleDrive) chunkUpload(ctx context.Context, file model.FileStreamer, 
 			up(float64(offset+chunkSize) / float64(file.GetSize()) * 100)
 			return nil
 		},
+			retry.Context(ctx),
 			retry.Attempts(3),
 			retry.DelayType(retry.BackOffDelay),
 			retry.Delay(time.Second))
